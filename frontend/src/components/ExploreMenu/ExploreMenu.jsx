378b6da26894f367a93bf5eaf909efa5bbc800f4
l00duck <nebojsanikolic1997@gmail.com>
2024-05-26 14:18:17 +0200

    food dispaly/ food item changes

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { menu_list } from "../../assets/assets";
+import "./ExploreMenu.css";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -12,13 +13,26 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div className="expllore-menu-list-item" key={index}>
-              <img src={item.menu_image} alt={item.menu_name} />
+            <div
+              onClick={() => {
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                );
+              }}
+              className="explore-menu-list-item"
+              key={index}
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt={item.menu_name}
+              />
               <p>{item.menu_name}</p>
             </div>
           );
         })}
       </div>
+      <hr />
     </div>
   );
 };
