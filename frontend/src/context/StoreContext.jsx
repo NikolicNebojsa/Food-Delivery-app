c2046f1feba2ebdd1b936c00c07bbe04cc2cf359
l00duck <nebojsanikolic1997@gmail.com>
2024-06-10 12:13:44 +0200

    cart and place order changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,10 +6,14 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
+  // Funkcija koja prima ID itema i dodaje ga u cart
   const addToCart = (itemId) => {
+    // ukoliko item sa odredjenim ID-em ne postoji u cartu, postavi broj itema sa tim ID-jem na 1
     if (!cartItems[itemId]) {
+      // setCartItems - funkcija koja menja broj cart itema u cartu. Prima callback funkciju i koristi spread operator da napravi novu kopiju prethodnog state-a i da doda item sa novim ID-em i postavi njegovu kolicinu na 1
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
+      // ista funkcija kao u redu iznad, samo osim sto postavlja kolicinu itema na 1, uzima kolicinu prethodnog state-a i povecava je za 1
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
   };
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -8,13 +8,14 @@ const PlaceOrder = () => {
   return (
     <form className="place-order">
       <div className="place-order-left">
-        <p className="title">Delivery Informatio</p>
+        <p className="title">Delivery Information</p>
         <div className="multi-fields">
           <input type="text" placeholder="First Name" />
           <input type="text" placeholder="Last Name" />
-          <input type="email" placeholder="Email address" />
-          <input type="text" placeholder="Street" />
         </div>
+
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="Street" />
         <div className="multi-fields">
           <input type="text" placeholder="City" />
           <input type="text" placeholder="State" />
